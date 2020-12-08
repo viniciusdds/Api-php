d01b0f9e8a91feaf8374fa7253d7c3d0048a481c
Vinicius Santos <vinicius.santos@aurora.local>
2020-12-08 18:54:21 -0300

    quinta commit

--- api.php
+++ api.php
@@ -9,6 +9,19 @@
 		return;
 	}
 	
+	$userAG = "core_11_1"; 
+	$passAG = "T3cn1c05p";
+	
+	//Aqui pego os dados do cliente na base da Alcis para inserir na tabela clienteAG do Mysql
+	$tnsAG = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.20.220.43)(PORT=1521)) (CONNECT_DATA=(SID=alcisagprd)))";
+	try {
+		$conAG = new PDO('oci:dbname='.$tnsAG,$userAG,$passAG);
+		$conAG->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//echo "Conectado AG<br>";
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+	
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -163,8 +176,8 @@
 					$mail->Host = "smtp.eadiaurora.com.br"; //host utilizado para o envio do email
 					$mail->Port = 587; //porta a ser utilizada
 					$mail->SMTPAuth = true; 
-					$mail->Username = "cadastro@eadiaurora.com.br"; //email de envio
-					$mail->Password= "!@root@!"; //senha do email
+					$mail->Username = "intranet_service@eadiaurora.com.br"; //email de envio
+					$mail->Password= "!@Root@!"; //senha do email
 					$mail->From = $emitente; 
 					$mail->FromName = "Redefinição de Senha"; 
 					$mail->AddAddress ($to); //
@@ -402,8 +415,279 @@
 			}else{
 				echo "3";
 			}
-		}
+		}	
+	}
+	
+	//Consulta os pedidos da LS200
+	if($action == "pesquisaPedido"){
+		$busca = $_REQUEST['busca'];
+		$cnpj = $_REQUEST['cnpj'];
+		
+		$pesq = $conAG->query("select 
+								dados.NOTA_FISCAL,
+								dados.LOTE_SERIAL,
+								dados.PRODUTO,
+								dados.QTD,
+								dados.LOTE,
+								dados.MEDIDA,
+								dados.UZ
+							from 
+							  (select 
+							  trunc(q.mng_frei + q.MNG_RES_AUF) QTD, 
+							  q.id_artikel PRODUTO, 
+							  q.nr_lieferschein NOTA_FISCAL,
+							  q.charge LOTE_SERIAL,
+							  q.TRENN_1 LOTE,
+							  q.NR_LE_1 UZ,
+							  p.prddf_mun MEDIDA
+							  from quanten q, PRDDF p, entow e 
+							  WHERE q.id_artikel = p.prddf_id
+							  and q.id_klient = e.entow_id
+							  and q.id_artikel not like '%KIT%'
+							  and (q.nr_lieferschein like '%$busca%'
+							  or q.id_artikel like '%$busca%'
+							  or q.TRENN_1 like '%$busca%')
+							  and e.entow_cll_cnpj = ".$cnpj."
+							  and rownum <= 1000
+							  group by 
+							  q.id_artikel, 
+							  q.nr_lieferschein,
+							  q.charge,
+							  q.TRENN_1,
+							  q.NR_LE_1,
+							  p.prddf_mun,
+							  q.mng_frei,
+							  q.MNG_RES_AUF
+							  order by q.nr_lieferschein, q.id_artikel) dados");
+							  
+		$id = 0;
+		$db_data = array();	
+        $db_cubagem = array();
+        $db_saldo = array();		
+        $pesq->execute();							
+		while($row = $pesq->fetch()){
+			$id = $id+1;
+			
+			//Consulta para pegar a cubagem
+			$condition = $conAG->query("select distinct a.vol CUBAGEM from ARTVPE a where a.typ_le != 'PP' and a.id_artikel = '".trim($row['PRODUTO'])."'");
+			$condition->execute();
+			$cub = $condition->fetch();
+			
+			//Pega o valor total
+			$qtotal = $conAG->query("select 
+										 trunc(sum(q.mng_frei) + sum(q.MNG_RES_AUF)) TOTAL, 
+									     q.id_artikel PRODUTO, 
+										 q.nr_lieferschein NOTA_FISCAL,
+									     q.charge LOTE_SERIAL,
+										 q.TRENN_1 LOTE,
+										 p.prddf_mun MEDIDA
+									from quanten q, PRDDF p 
+									  WHERE q.id_artikel = p.prddf_id
+									  and q.nr_lieferschein like '%".trim($row['NOTA_FISCAL'])."%'
+									  and q.id_artikel like '%".trim($row['PRODUTO'])."%'
+									  and q.charge like '%".trim($row['LOTE_SERIAL'])."%'
+									  group by id_artikel,q.nr_lieferschein,q.charge,q.TRENN_1,p.prddf_mun order by q.nr_lieferschein");
+			
+			$qtotal->execute();
+			$total = $qtotal->fetch();	
+			
+			//Verifico quantas linhas ainda restão na base Mysql
+			$countEstoque = mysqli_query($con,"SELECT * FROM sistemas_ag.clientes_ag where nota_fiscal = '".$row['NOTA_FISCAL']."' and produto = '".$row['PRODUTO']."' group by num_pedido")or die("erro no select countEstoque");
+			$EstoqueLine = mysqli_num_rows($countEstoque);
+			
+			//Consulta as UZ em estoque
+			$verificaUZ = mysqli_query($con,"select palete, pedido from sistemas_ag.clientes_ag where palete = '".trim($row['UZ'])."'")or die("erro no select verifica UZ");
+			$linhasUZ = mysqli_num_rows($verificaUZ);
+			$pedidoUZ = mysqli_fetch_array($verificaUZ);
+			
+			//Verifico se já foi quebrado a quantidade que estava em palete
+			if($linhasUZ > 0){
+				$clausula = " and a.palete = '".trim($row['UZ'])."' ";	
+			}else{
+				$clausula = " and  a.palete = '--' "; 
+			}
+			$groupBy = "group by a.palete,troca";
+			
+			//Verifica se tem algum registro no banco
+			$teste = mysqli_query($con,"select 
+											sum(pedido) as pedidos,
+                                            qtd_disp disp,
+											pedido,
+                                            qtd_disp - sum(pedido) sobra,
+											palete,
+											troca,
+											case when qtd_diff is not null then
+											
+											case when sum(qtd_diff) > sum(pedido) 
+												then 
+													(sum(qtd_diff)-sum(pedido))
+												when  sum(qtd_diff) < sum(pedido) 
+												then 
+													(sum(pedido)-sum(qtd_diff)) 
+												else 
+													(select qtd_diff from sistemas_ag.clientes_ag where nota_fiscal = a.nota_fiscal and produto = a.produto and lote = a.lote and qtd_diff is not null order by time_stamp desc limit 1)
+												end
+											else
+												0
+											end qtd_diff,
+											
+											
+											case when palete = '".$row['UZ']."' then	
+												case when pedido < ".$row['QTD']." and palete = '".$row['UZ']."'
+												then
+													1
+												else
+													0
+												end
+											else
+												0
+											end testando,
+											qtd_por_uz,
+                                            count(*) as linhas
+										from sistemas_ag.clientes_ag a 
+											where
+											nota_fiscal = '".trim($row['NOTA_FISCAL'])."' and lote_serial = '".trim($row['LOTE_SERIAL'])."' 
+											and a.produto = '".trim($row['PRODUTO'])."' and a.lote = '".trim($row['LOTE'])."' ".$clausula." 
+											")or die("erro no select coleta cliente mysql q total");
+						
+			$rows = mysqli_num_rows($teste);
+			
+			//Aqui eu pego os valores acumulados na base Mysql para decrementar com os valores vindos do estoque, desconsiderando os valores pedido por PP
+			$acumulados = mysqli_query($con,"select 
+												sum(pedido) total,
+												count(*) cont,
+												(select qtd_diff from sistemas_ag.clientes_ag where nota_fiscal = a.nota_fiscal and produto = a.produto and lote = a.lote order by time_stamp desc limit 1) diff
+											  from sistemas_ag.clientes_ag a
+												where nota_fiscal = '".$row['NOTA_FISCAL']."'
+												and produto = '".$row['PRODUTO']."'
+												and lote = '".$row['LOTE']."'
+												and pedido != qtd_por_uz");
+			$acumulado = mysqli_fetch_array($acumulados);
+			
+			//Aqui Verifico quais pedido foram pedidos por quantidade PP
+			$EstoqueUZ = mysqli_query($con,"select palete from sistemas_ag.clientes_ag where nota_fiscal = '".$row['NOTA_FISCAL']."' and produto = '".$row['PRODUTO']."' and lote = '".$row['LOTE']."' and pedido = qtd_por_uz");
+			
+			//Trata os valores que foram pedidos avulsos na tela quantidade disponivel em estoque para quebrar a quantidade em UZs
+				if($rows > 0){
+					$valor = mysqli_fetch_array($teste);
+					extract($valor);
+					
+					
+					$test = array();
+					while($countUZ = mysqli_fetch_array($EstoqueUZ)){
+						$test[] = $countUZ['palete'];
+					}
+					
+						
+                     						
+					if(!in_array($row['UZ'],$test)){	
+						$arr[] = $row['NOTA_FISCAL'];
+						$teste = array_count_values($arr);
+						foreach($teste as $v1){
+						}	
+								
+								
+						if($v1 == 1){
+							$estoque = $row['QTD'] - $acumulado['total'];		
+						}else{	
+							if($estoque < 0){
+								$estoque = $row['QTD'] + $estoque;
+							}else{	
+								$estoque = $row['QTD'];
+							}
+						}
+					}
+				}else{
+					$pedidos = 0;
+					$palete = 0;
+					$estoque = $row['QTD'];	
+				}
+				
+				if($total['TOTAL'] != $pedidos){
+					//Verifica se a quantidade existe em estoque
+					if(round($estoque) <= 0 || in_array($row['UZ'],$test)){
+						if($id == 1){
+							$aviso = 0;	
+						}		
+					}else{			
+						$aviso = 1;
+						
+						if(substr($cub['CUBAGEM'],0,1) == ","){
+							$cubagem = "0".$cub['CUBAGEM'];
+						}else{
+							$cubagem = $cub['CUBAGEM'];
+						}
+							
+						
+							$ars1[] = $id;
+							$x = 0;
+									
+							if($estoque != $row['QTD']){
+								foreach($ars1 as $v4){
+									if($x == 0){
+										if($id == $ars1[$x]){
+											//retorno da consulta
+											$db_cubagem[] = $cubagem;
+											$db_saldo[] = round($estoque);
+											$db_data[] = $row;
+											
+										}else{
+											//retorno da consulta
+											$db_cubagem[] = $cubagem;
+											$db_saldo[] = round($estoque);
+											$db_data[] = $row;
+										}
+									}
+									$x++;
+								}
+							}else{		
+								//retorno da consulta
+								$db_cubagem[] = $cubagem;
+								$db_saldo[] = round($estoque);
+								$db_data[] = $row;							
+							}
+											
+								
+								$ars[] = $id;
+								$i = 0;
+												
+								if($estoque != $row['QTD']){
+									foreach($ars as $v3){
+										if($i == 0){
+											if($id == $ars[$i]){	
+												$db_saldo[] = round($estoque);
+												$db_data[] = $row;
+											}else{
+												$db_saldo[] = round($estoque);
+												$db_data[] = $row;
+											}
+										}
+										$i++;
+									}
+								}else{
+									$db_saldo[] = round($estoque);
+									$db_data[] = $row;
+								}
+									
+									if($cubagem){
+										$db_cubagem[] = $cubagem;
+									}else{
+										$db_cubagem[] = 'PP';
+									}
+							
+					}
+				}else{
+					if($id == 1){
+						$aviso = 0;
+					}
+				}
+			
+		} // fim do while
+		
+		echo json_encode($db_data);
+		
 		
 	}
+	
 		
 ?>
