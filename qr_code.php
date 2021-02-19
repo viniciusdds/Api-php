f3af6ffab2359588637a2fc6239619cfb3975fda
Vinicius Santos <vinicius.santos@aurora.local>
2021-02-19 18:40:47 -0300

    commit 32

---
+++ qr_code.php
@@ -0,0 +1,4 @@
+<?php 
+
+include("phpqrcode/qrlib.php");
+QRcode::png("http://10.101.10.22:8080/camera/AuroraEadi.apk");
---
+++ qr_code2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmls="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
+<head>
+	<title>QrCode PHP</title>
+</head>
+
+	<body>
+		<h1>Criando QrCode no PHP</h1>
+		<?php 
+			$aux = 'qr_img/php/qr_img.php?';
+			$aux .= 'd=http://10.101.10.22:8080/camera/AuroraEadi.apk&';
+			$aux .= 'e=H&';
+			$aux .= 's=2&';
+			$aux .= 't=P&';
+		?>
+		<img src="<?php echo $aux; ?>";
+	</body>
+</html>
